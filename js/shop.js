7c121a4dd5e0c79fb31e99bc6da520dfeefe3dbb
xFloppa <92061815+xFloppa@users.noreply.github.com>
2022-06-06 19:46:39 +0200

    Update shop.js

--- js/shop.js
+++ js/shop.js
@@ -1,23 +1,23 @@
 function Vip1() {
-    window.location.href = "https://github.com/Tiagox42/Minecraft-WebSite";
+    window.location.href = "https://github.com/xFloppa/floppa-site";
 }
 
 function Vip2() {
-    window.location.href = "https://github.com/Tiagox42/Minecraft-WebSite";
+    window.location.href = "https://github.com/xFloppa/floppa-site";
 }
 
 function Vip3() {
-    window.location.href = "https://github.com/Tiagox42/Minecraft-WebSite";
+    window.location.href = "https://github.com/xFloppa/floppa-site";
 }
 
 function Vip4() {
-    window.location.href = "https://github.com/Tiagox42/Minecraft-WebSite";
+    window.location.href = "https://github.com/xFloppa/floppa-site";
 }
 
 function Vip5() {
-    window.location.href = "https://github.com/Tiagox42/Minecraft-WebSite";
+    window.location.href = "https://github.com/xFloppa/floppa-site";
 }
 
 function Vip6() {
-    window.location.href = "https://github.com/Tiagox42/Minecraft-WebSite";
+    window.location.href = "https://github.com/xFloppa/floppa-site";
 }
